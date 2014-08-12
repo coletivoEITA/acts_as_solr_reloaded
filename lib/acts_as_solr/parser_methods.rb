4fc7c089cbc00b650cefc428f082d33fb4be0fcb
Braulio Bhavamitra <braulio@eita.org.br>
2014-08-12 17:13:37 -0300

    Add ngram_text type and configure of query_fields and default_field

--- lib/acts_as_solr/parser_methods.rb
+++ lib/acts_as_solr/parser_methods.rb
@@ -4,12 +4,16 @@ module ActsAsSolr #:nodoc:
 
     # Method used by mostly all the ClassMethods when doing a search
     def parse_query(query=nil, options={})
-      valid_options = [:models, :lazy, :core, :results_format, :sql_options,
+      valid_options = [
+        :models, :lazy, :core, :results_format, :sql_options,
         :alternate_query, :boost_functions, :filter_queries, :facets, :sort,
         :scores, :operator, :latitude, :longitude, :radius, :relevance, :highlight,
-        :offset, :per_page, :limit, :page,]
+        :offset, :per_page, :limit, :page,
+        :query_fields, :default_field,
+      ]
       query_options = {}
       options[:results_format] ||= :objects
+      options[:default_field] ||= 'text'
 
       return if query.nil?
       raise "Query should be a string" unless query.is_a?(String)
@@ -49,77 +53,95 @@ module ActsAsSolr #:nodoc:
         query_options[:rows] = per_page
         query_options[:start] = offset
 
-        query_options[:operator] = options[:operator]
-
         query_options[:boost_functions] = replace_types([*options[:boost_functions]], '').join(' ') if options[:boost_functions]
 
-        # first steps on the facet parameter processing
-        if options[:facets]
-          query_options[:facets] = {}
-          query_options[:facets][:limit] = -1  # TODO: make this configurable
-          query_options[:facets][:sort] = :count if options[:facets][:sort]
-          query_options[:facets][:mincount] = 0
-          query_options[:facets][:mincount] = 1 if options[:facets][:zeros] == false
-          # override the :zeros (it's deprecated anyway) if :mincount exists
-          query_options[:facets][:mincount] = options[:facets][:mincount] if options[:facets][:mincount]
-          query_options[:facets][:fields] = options[:facets][:fields].map{ |k| "#{k}_facet" } if options[:facets][:fields]
-          query_options[:filter_queries] += replace_types([*options[:facets][:browse]]) if options[:facets][:browse]
-          query_options[:facets][:queries] = replace_types([*options[:facets][:query]]) if options[:facets][:query]
+        parse_facets query_options, options
+        parse_highlight query_options, options
+        parse_sort query_options, options
+        parse_location query_options, options
+        parse_query_fields query_options, options
 
-          if options[:facets][:dates]
-            query_options[:date_facets] = {}
-            # if options[:facets][:dates][:fields] exists then :start, :end, and :gap must be there
-            if options[:facets][:dates][:fields]
-              [:start, :end, :gap].each { |k| raise "#{k} must be present in faceted date query" unless options[:facets][:dates].include?(k) }
-              query_options[:date_facets][:fields] = []
-              options[:facets][:dates][:fields].each { |f|
-                if f.kind_of? Hash
-                  key = f.keys[0]
-                  query_options[:date_facets][:fields] << {"#{key}_d" => f[key]}
-                  validate_date_facet_other_options(f[key][:other]) if f[key][:other]
-                else
-                  query_options[:date_facets][:fields] << "#{f}_d"
-                end
-              }
-            end
-
-            query_options[:date_facets][:start]   = options[:facets][:dates][:start] if options[:facets][:dates][:start]
-            query_options[:date_facets][:end]     = options[:facets][:dates][:end] if options[:facets][:dates][:end]
-            query_options[:date_facets][:gap]     = options[:facets][:dates][:gap] if options[:facets][:dates][:gap]
-            query_options[:date_facets][:hardend] = options[:facets][:dates][:hardend] if options[:facets][:dates][:hardend]
-            query_options[:date_facets][:filter]  = replace_types([*options[:facets][:dates][:filter]].collect{|k| "#{k.dup.sub!(/ *:(?!\d) */,"_d:")}"}) if options[:facets][:dates][:filter]
-
-            if options[:facets][:dates][:other]
-              validate_date_facet_other_options(options[:facets][:dates][:other])
-              query_options[:date_facets][:other] = options[:facets][:dates][:other]
-            end
-
-          end
-        end
-
-        if options[:highlight]
-          query_options[:highlighting] = {}
-          query_options[:highlighting][:field_list] = replace_types([*options[:highlight][:fields]], '') if options[:highlight][:fields]
-          query_options[:highlighting][:require_field_match] =  options[:highlight][:require_field_match] if options[:highlight][:require_field_match]
-          query_options[:highlighting][:max_snippets] = options[:highlight][:max_snippets] if options[:highlight][:max_snippets]
-          query_options[:highlighting][:prefix] = options[:highlight][:prefix] if options[:highlight][:prefix]
-          query_options[:highlighting][:suffix] = options[:highlight][:suffix] if options[:highlight][:suffix]
-        end
-
-        query_options[:sort] = replace_types([*options[:sort]], '')[0] if options[:sort]
-
-        if options[:radius]
-          query_options[:radius] = options[:radius]
-          query_options[:filter_queries] << '{!geofilt}'
-        end
-        query_options[:latitude] = options[:latitude]
-        query_options[:longitude] = options[:longitude]
-
+        query_options[:operator] = options[:operator]
         not_dismax = query_options[:operator] == :or
-        request = not_dismax ? Solr::Request::Standard.new(query_options) : Solr::Request::Dismax.new(query_options)
-        ActsAsSolr::Post.execute(request, options[:core])
+        request = if not_dismax then Solr::Request::Standard.new(query_options) else Solr::Request::Dismax.new query_options end
+        ActsAsSolr::Post.execute request, options[:core]
       rescue
         raise "#{$query} There was a problem executing your search\n#{query_options.inspect}\n: #{$!} in #{$!.backtrace.first}"
+      end
+    end
+
+    def parse_query_fields query_options, options
+      options[:query_fields] ||= []
+      query_options[:query_fields] = replace_types([*options[:query_fields]], '').join ' '
+    end
+
+    def parse_sort query_options, options
+      query_options[:sort] = replace_types([*options[:sort]], '')[0] if options[:sort]
+    end
+
+    def parse_location query_options, options
+      if options[:radius]
+        query_options[:radius] = options[:radius]
+        query_options[:filter_queries] << '{!geofilt}'
+      end
+      query_options[:latitude] = options[:latitude]
+      query_options[:longitude] = options[:longitude]
+    end
+
+    def parse_highlight query_options, options
+      if options[:highlight]
+        query_options[:highlighting] = {}
+        query_options[:highlighting][:field_list] = replace_types([*options[:highlight][:fields]], '') if options[:highlight][:fields]
+        query_options[:highlighting][:require_field_match] =  options[:highlight][:require_field_match] if options[:highlight][:require_field_match]
+        query_options[:highlighting][:max_snippets] = options[:highlight][:max_snippets] if options[:highlight][:max_snippets]
+        query_options[:highlighting][:prefix] = options[:highlight][:prefix] if options[:highlight][:prefix]
+        query_options[:highlighting][:suffix] = options[:highlight][:suffix] if options[:highlight][:suffix]
+      end
+    end
+
+    def parse_facets query_options, options
+      # first steps on the facet parameter processing
+      if options[:facets]
+        query_options[:facets] = {}
+        query_options[:facets][:limit] = -1  # TODO: make this configurable
+        query_options[:facets][:sort] = :count if options[:facets][:sort]
+        query_options[:facets][:mincount] = 0
+        query_options[:facets][:mincount] = 1 if options[:facets][:zeros] == false
+        # override the :zeros (it's deprecated anyway) if :mincount exists
+        query_options[:facets][:mincount] = options[:facets][:mincount] if options[:facets][:mincount]
+        query_options[:facets][:fields] = options[:facets][:fields].map{ |k| "#{k}_facet" } if options[:facets][:fields]
+        query_options[:filter_queries] += replace_types([*options[:facets][:browse]]) if options[:facets][:browse]
+        query_options[:facets][:queries] = replace_types([*options[:facets][:query]]) if options[:facets][:query]
+
+        if options[:facets][:dates]
+          query_options[:date_facets] = {}
+          # if options[:facets][:dates][:fields] exists then :start, :end, and :gap must be there
+          if options[:facets][:dates][:fields]
+            [:start, :end, :gap].each { |k| raise "#{k} must be present in faceted date query" unless options[:facets][:dates].include?(k) }
+            query_options[:date_facets][:fields] = []
+            options[:facets][:dates][:fields].each { |f|
+              if f.kind_of? Hash
+                key = f.keys[0]
+                query_options[:date_facets][:fields] << {"#{key}_d" => f[key]}
+                validate_date_facet_other_options(f[key][:other]) if f[key][:other]
+              else
+                query_options[:date_facets][:fields] << "#{f}_d"
+              end
+            }
+          end
+
+          query_options[:date_facets][:start]   = options[:facets][:dates][:start] if options[:facets][:dates][:start]
+          query_options[:date_facets][:end]     = options[:facets][:dates][:end] if options[:facets][:dates][:end]
+          query_options[:date_facets][:gap]     = options[:facets][:dates][:gap] if options[:facets][:dates][:gap]
+          query_options[:date_facets][:hardend] = options[:facets][:dates][:hardend] if options[:facets][:dates][:hardend]
+          query_options[:date_facets][:filter]  = replace_types([*options[:facets][:dates][:filter]].collect{|k| "#{k.dup.sub!(/ *:(?!\d) */,"_d:")}"}) if options[:facets][:dates][:filter]
+
+          if options[:facets][:dates][:other]
+            validate_date_facet_other_options(options[:facets][:dates][:other])
+            query_options[:date_facets][:other] = options[:facets][:dates][:other]
+          end
+
+        end
       end
     end
 
